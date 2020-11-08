51397f20be2849df658e4fc777d40dd7ed80a27b
Margokomilfo-dev <margokomilfo@mail.ru>
2020-11-08 10:13:25 +0100

    Updates

---
+++ precache-manifest.f2e5fd48aebedc1ccb7c58020029f6d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a02bbea313b0a6b87adc91330d9445",
+    "url": "/it-incubator-todolist/index.html"
+  },
+  {
+    "revision": "cb6838f0d48b9f34ef5a",
+    "url": "/it-incubator-todolist/static/css/main.5f710a33.chunk.css"
+  },
+  {
+    "revision": "3a630a4377370057f2e0",
+    "url": "/it-incubator-todolist/static/js/2.ac49d601.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/it-incubator-todolist/static/js/2.ac49d601.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb6838f0d48b9f34ef5a",
+    "url": "/it-incubator-todolist/static/js/main.cb080620.chunk.js"
+  },
+  {
+    "revision": "533a293a5b5cf19edc48",
+    "url": "/it-incubator-todolist/static/js/runtime-main.2a8635ae.js"
+  }
+]);
