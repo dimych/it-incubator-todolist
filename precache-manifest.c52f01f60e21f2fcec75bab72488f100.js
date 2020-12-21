5889ded07e63a8584bcd64d50f5fd5d49a50e564
Margokomilfo-dev <margokomilfo@mail.ru>
2020-12-21 19:48:00 +0100

    Updates

---
+++ precache-manifest.c52f01f60e21f2fcec75bab72488f100.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8b47ee040de8514a5f1f6ce7f69ea6",
+    "url": "/it-incubator-todolist/index.html"
+  },
+  {
+    "revision": "d1d2f7fd8f96d752f0e2",
+    "url": "/it-incubator-todolist/static/css/main.fbcc45c1.chunk.css"
+  },
+  {
+    "revision": "cd2fbed31528735ec470",
+    "url": "/it-incubator-todolist/static/js/2.a7cb6595.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/it-incubator-todolist/static/js/2.a7cb6595.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1d2f7fd8f96d752f0e2",
+    "url": "/it-incubator-todolist/static/js/main.be63afad.chunk.js"
+  },
+  {
+    "revision": "533a293a5b5cf19edc48",
+    "url": "/it-incubator-todolist/static/js/runtime-main.2a8635ae.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/it-incubator-todolist/precache-manifest.bdd0e6880168bf8e3b7e9278543fcec1.js"
+  "/it-incubator-todolist/precache-manifest.c52f01f60e21f2fcec75bab72488f100.js"
 );
 
 self.addEventListener('message', (event) => {
